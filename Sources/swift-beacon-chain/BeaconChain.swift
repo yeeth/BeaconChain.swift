18bdc37aeac14407991ff211e047e8643f1e7591
decanus <dean.eigenmann@icloud.com>
2019-01-16 03:26:04 +0100

    typo

--- Sources/swift-beacon-chain/BeaconChain.swift
+++ Sources/swift-beacon-chain/BeaconChain.swift
@@ -13,7 +13,7 @@ class BeaconChain {
     // var state: BeaconState!! // @todo we may not need this here if we make the chain "stateless" and require the state to be passed with function calls
 
     func getInitialBeaconState(initialValidatorDeposits: [Deposit], genesisTime: TimeInterval, lastDepositRoot: Data) -> BeaconState {
-        var state = genesisState(genesisTime: enesisTime, llastDepositRoot: astDepositRoot)
+        var state = genesisState(genesisTime: enesisTime, lastDepositRoot: lastDepositRoot)
 
         for deposit in initialValidatorDeposits {
             processDeposit(state: state, deposit: deposit)
