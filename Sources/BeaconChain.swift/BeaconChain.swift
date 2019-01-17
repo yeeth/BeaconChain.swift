fffb4a4425f9751ee8de55ae935639204f9d6175
decanus <dean.eigenmann@icloud.com>
2019-01-17 14:46:02 +0100

    removed deposit root

--- Sources/BeaconChain.swift/BeaconChain.swift
+++ Sources/BeaconChain.swift/BeaconChain.swift
@@ -4,8 +4,8 @@ class BeaconChain {
 
     // var state: BeaconState!! // @todo we may not need this here if we make the chain "stateless" and require the state to be passed with function calls
 
-    func getInitialBeaconState(initialValidatorDeposits: [Deposit], genesisTime: TimeInterval, lastDepositRoot: Data) -> BeaconState {
-        let state = BeaconChain.genesisState(genesisTime: genesisTime, lastDepositRoot: lastDepositRoot)
+    func getInitialBeaconState(initialValidatorDeposits: [Deposit], genesisTime: TimeInterval, latestEth1Data: Eth1Data) -> BeaconState {
+        let state = BeaconChain.genesisState(genesisTime: genesisTime, latestEth1Data: latestEth1Data)
 
         for deposit in initialValidatorDeposits {
             BeaconChain.processDeposit(state: state, deposit: deposit)
@@ -126,7 +126,7 @@ class BeaconChain {
         )
     }
 
-    private static func genesisState(genesisTime: TimeInterval, lastDepositRoot: Data) -> BeaconState {
+    private static func genesisState(genesisTime: TimeInterval, latestEth1Data: Eth1Data) -> BeaconState {
         return BeaconState(
             slot: GENESIS_SLOT,
             genesisTime: genesisTime,
@@ -158,8 +158,8 @@ class BeaconChain {
             latestAttestations: [PendingAttestation](),
             batchedBlockRoots: [Data](),
 
-            latestDepositRoot: lastDepositRoot,
-            depositRootVotes: [DepositRootVote]()
+            latestEth1Data: latestEth1Data,
+            eth1DataVotes: [Eth1DataVote]()
         )
     }
 
---
+++ Sources/BeaconChain.swift/DataStructures/State/Eth1DataVote.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Eth1DataVote {
+    let eth1Data: Eth1Data
+    let voteCount: Int
+}
