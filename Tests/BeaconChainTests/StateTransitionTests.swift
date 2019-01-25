fa1e7bc53e395ec46013978690f8ff30f967d39e
Dean Eigenmann <dean@tokenate.io>
2019-01-25 17:51:35 +0100

    test/expectedBeaconChainHead (#39)

--- Tests/BeaconChainTests/StateTransitionTests.swift
+++ Tests/BeaconChainTests/StateTransitionTests.swift
@@ -155,4 +155,56 @@ final class StateTransitionTests: XCTestCase {
             }
         }
     }
+
+    func testExpectedBeaconChainHead() {
+        // @todo check these numbers
+        let tests = [
+            ([Int](), [31999427550, 31999427550, 31999427550, 31999427550]),
+            ([0,1], [32000286225, 32000286225, 31999427550, 31999427550]),
+            ([0,1,2,3], [32000572450, 32000572450, 32000572450, 32000572450])
+            ] as [([Int], [UInt64])]
+
+        for test in tests {
+            let state = BeaconChain.genesisState(
+                genesisTime: TimeInterval(0),
+                latestEth1Data: Eth1Data(depositRoot: Data(count: 32), blockHash: Data(count: 32))
+            )
+
+            for _ in 0..<4 {
+                state.validatorBalances.append(MAX_DEPOSIT_AMOUNT)
+                state.validatorRegistry.append(
+                    Validator(
+                        pubkey: Data(count: 32),
+                        withdrawalCredentials: Data(count: 32),
+                        randaoCommitment: Data(count: 32),
+                        randaoLayers: 0,
+                        activationSlot: 0,
+                        exitSlot: FAR_FUTURE_SLOT,
+                        withdrawalSlot: 0,
+                        penalizedSlot: 0,
+                        exitCount: 0,
+                        statusFlags: 0,
+                        custodyCommitment: Data(count: 32),
+                        latestCustodyReseedSlot: 0,
+                        penultimateCustodyReseedSlot: 0
+                    )
+                )
+            }
+
+            let totalBalance = MAX_DEPOSIT_AMOUNT * UInt64(test.1.count)
+
+            let newState = StateTransition.expectedBeaconChainHead(
+                state: state,
+                previousEpochHeadAttesterIndices: test.0,
+                activeValidators: Set([0,1,2,3]),
+                previousEpochHeadAttestingBalance: MAX_DEPOSIT_AMOUNT * UInt64(test.0.count),
+                baseRewardQuotient: StateTransition.baseRewardQuotient(totalBalance: totalBalance),
+                totalBalance: totalBalance
+            )
+
+            for (i, balance) in test.1.enumerated() {
+                XCTAssertEqual(balance, newState.validatorBalances[i])
+            }
+        }
+    }
 }
