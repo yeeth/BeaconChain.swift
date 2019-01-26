c4316245f57b9c24eee565765bd30c59991e24aa
decanus <dean.eigenmann@icloud.com>
2019-01-26 03:01:01 +0100

    init node

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "BeaconChain",
-            targets: ["BeaconChain"])
+            targets: ["BeaconChain"]),
+        .executable(
+            name: "Node",
+            targets: ["Node"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +26,12 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "BeaconChainTests",
-            dependencies: ["BeaconChain"])
+            dependencies: ["BeaconChain"]),
+        .target(
+            name: "Node",
+            dependencies: []),
+        .testTarget(
+            name: "NodeTests",
+            dependencies: ["Node"])
     ]
 )
