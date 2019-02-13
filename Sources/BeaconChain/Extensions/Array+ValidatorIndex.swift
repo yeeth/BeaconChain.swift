deefbe33dc3eda94df7cbba11067db06a1167455
Dean Eigenmann <dean.eigenmann@icloud.com>
2019-02-13 13:50:26 +0100

    Update Array+ValidatorIndex.swift

--- Sources/BeaconChain/Extensions/Array+ValidatorIndex.swift
+++ Sources/BeaconChain/Extensions/Array+ValidatorIndex.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Array where Element == ValidatorIndex {
 
     func totalBalance(state: BeaconState) -> Gwei {
-        return self.map {
+        return map {
             return BeaconChain.getEffectiveBalance(state: state, index: $0)
         }
         .reduce(0, +)
