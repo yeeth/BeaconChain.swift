e59ae18679944c75d6d890d3474a2ae988e45cf4
Dean Eigenmann <dean@tokenate.io>
2019-02-13 05:02:37 +0100

    refactor/operator-specific-classes (#50)

--- Sources/BeaconChain/Operators/Int.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Int {
-
-    static func ** (radix: Int, power: Int) -> Int {
-        return Int(pow(Double(radix), Double(power)))
-    }
-}
-
--- Sources/BeaconChain/Operators/Power.swift
+++ Sources/BeaconChain/Operators/Power.swift
@@ -10,3 +10,9 @@ extension UInt64 {
     }
 }
 
+extension Int {
+
+    static func ** (radix: Int, power: Int) -> Int {
+        return Int(pow(Double(radix), Double(power)))
+    }
+}
