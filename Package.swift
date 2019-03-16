4255dee1e1b7e44b9c8c9714a4d72975d7fc9493
decanus <dean.eigenmann@icloud.com>
2019-03-16 17:16:58 +0100

    init HobbitNode

--- Package.swift
+++ Package.swift
@@ -9,11 +9,15 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "BeaconChain",
-            targets: ["BeaconChain"])
+            targets: ["BeaconChain"]),
+        .executable(
+            name: "HobbitNode",
+            targets: ["HobbitNode"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/yeeth/Hobbits.swift.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,6 +25,10 @@ let package = Package(
         .target(
             name: "BeaconChain",
             dependencies: []),
+        .target(
+            name: "HobbitNode",
+            dependencies: ["Hobbits"]
+        ),
         .testTarget(
             name: "BeaconChainTests",
             dependencies: ["BeaconChain"])
