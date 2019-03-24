1f957684513ed7f635cf711453fddbea08a8e462
Dean Eigenmann <dean@tokenate.io>
2019-03-24 15:01:41 +0100

    refactor/attestation-target (#79)
    * moved attestationTargets function to array extension
    
    * renaming
    
    * updated
    
    * Update Types.swift
    
    * fix indent
    
    * cleaned up votes function. added test
    
    * added tests
    
    * fixed

--- Sources/BeaconChain/ForkChoice/LMDGhost.swift
+++ Sources/BeaconChain/ForkChoice/LMDGhost.swift
@@ -3,7 +3,7 @@ import Foundation
 class LMDGhost: ForkChoice {
 
     func execute(store: Store, startState: BeaconState, startBlock: BeaconBlock) -> BeaconBlock {
-        let attestationTargets = startState.validatorRegistry.activeIndices(epoch: startState.slot.toEpoch()).map {
+        let targets = startState.validatorRegistry.activeIndices(epoch: startState.slot.toEpoch()).map {
             ($0, store.latestAttestationTarget(validator: $0))
         }
 
@@ -15,29 +15,8 @@ class LMDGhost: ForkChoice {
             }
 
             head = children.max {
-                voteCount(store: store, state: startState, block: $0, attestationTargets: attestationTargets) < voteCount(store: store, state: startState, block: $1, attestationTargets: attestationTargets)
+                targets.votes(store: store, state: startState, block: $0) < targets.votes(store: store, state: startState, block: $1)
             }!
         }
-    }
-
-    private func voteCount(
-        store: Store,
-        state: BeaconState,
-        block: BeaconBlock,
-        attestationTargets: [(ValidatorIndex, BeaconBlock)]
-    ) -> UInt64 {
-        return attestationTargets.compactMap {
-            (index, target) in
-            guard let ancestor = store.ancestor(block: target, slot: index) else {
-                return nil
-            }
-
-            if ancestor == block {
-                return BeaconChain.getEffectiveBalance(state: state, index: index) / FORK_CHOICE_BALANCE_INCREMENT
-            }
-
-            return nil
-        }
-        .reduce(0, +)
     }
 }
