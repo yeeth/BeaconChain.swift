415ad73f480f8c75e432d9aaa935e4f0e086210f
decanus <dean.eigenmann@icloud.com>
2019-07-01 23:14:23 +0200

    types

---
+++ Sources/BeaconChain/Types.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+typealias Slot = UInt64
+typealias Epoch = UInt64
+typealias Shard = UInt64
+typealias ValidatorIndex = UInt64
+typealias Gwei = UInt64
+typealias Hash = Data
+typealias Version = UInt32
+typealias DomainType = UInt32
+typealias Domain = UInt64
+typealias BLSPubKey = Data
+typealias BLSSignature = Data
