8509f76105d98109224edbcd8f86b17d5b9b9144
decanus <dean.eigenmann@icloud.com>
2019-07-02 02:17:48 +0200

    disable rule

--- Tests/BeaconChainTests/Operators/PowerTests.swift
+++ Tests/BeaconChainTests/Operators/PowerTests.swift
@@ -3,13 +3,14 @@ import XCTest
 
 class PowerTests: XCTestCase {
 
+    // swiftlint:disable large_tuple
     let tests: [(base: Int, exponent: Int, expected: Int)] = [
         (2, 0, 1),
         (2, 1, 2),
         (2, 2, 4),
         (2, 3, 8),
-        (2, 4, 16),
-    ];
+        (2, 4, 16)
+    ]
 
     func testPowerForInt() {
         for test in tests {
