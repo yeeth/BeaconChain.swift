9b881811f80d2ac60e68aec4814f112d8a734257
decanus <dean.eigenmann@icloud.com>
2019-07-03 15:34:52 -0400

    public

--- Sources/BeaconChain/DepositData.swift
+++ Sources/BeaconChain/DepositData.swift
@@ -4,15 +4,15 @@ import Foundation
 public struct DepositData {
 
     /// BLS12-381 pubkey to be used to sign messages by the validator.
-    let pubkey: BLSPubKey
+    public let pubkey: BLSPubKey
 
     /// The hash of an offline pubkey to be used to withdraw the staked funds after exiting.
     /// This key is not used actively in validation and can be kept in cold storage.
-    let withdrawalCredentials: Hash
+    public let withdrawalCredentials: Hash
 
     /// Amount in Gwei that was deposited.
-    let amount: Gwei
+    public let amount: Gwei
 
     /// This is used as a one-time “proof of custody” required for securely using BLS keys.
-    let signature: BLSSignature
+    public let signature: BLSSignature
 }
