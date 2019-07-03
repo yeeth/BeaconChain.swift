f55ff2297b89ac30b4cee5500b3bb8c27142b271
decanus <dean.eigenmann@icloud.com>
2019-07-03 15:33:24 -0400

    added docs

--- Sources/BeaconChain/Enums/DomainType.swift
+++ Sources/BeaconChain/Enums/DomainType.swift
@@ -1,5 +1,6 @@
 import Foundation
 
-enum DomainType: UInt32 {
+/// A signature domain type.
+public enum DomainType: UInt32 {
     case beaconProposer, randao, attestation, deposit, voluntaryExit, transfer
 }
