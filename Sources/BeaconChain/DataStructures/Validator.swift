e9cf06e9a754a6e8f0ae34b8ff40dd6e352e8131
decanus <dean.eigenmann@icloud.com>
2019-07-19 14:25:22 -0500

    validator predicates

--- Sources/BeaconChain/DataStructures/Validator.swift
+++ Sources/BeaconChain/DataStructures/Validator.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 struct Validator {
+
     let pubkey: BLSPubKey
     let withdrawalCredentials: Hash
     let effectiveBalance: Gwei
@@ -9,4 +10,20 @@ struct Validator {
     let activationEpoch: Epoch
     let exitEpoch: Epoch
     let withdrawableEpoch: Epoch
+
+    /// Checks if a `validator` is active.
+    ///
+    /// - Parameters:
+    ///     - epoch: The given epoch to check activation for.
+    func isActive(epoch: Epoch) -> Bool {
+        return activationEpoch <= epoch && epoch < exitEpoch
+    }
+
+    /// Checks if a `validator` is slashable.
+    ///
+    /// - Parameters:
+    ///     - epoch: The given epoch to check slashability for.
+    func isSlashable(epoch: Epoch) -> Bool {
+        return !slashed && activationEpoch <= epoch && epoch < withdrawableEpoch
+    }
 }
